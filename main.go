42f049380d77ae298bf6bda0f072829553b77513
Vitiosum <loic.piron@clever-cloud.com>
2026-03-11 13:51:42 +0100

    Implement health check and update index page
    Added health check endpoint and updated index page response.

--- main.go
+++ main.go
@@ -3,19 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", indexPage)
+	http.HandleFunc("/health", healthCheck)
+
 	http.ListenAndServe("0.0.0.0:8080", nil)
 }
 
 func indexPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(
-		w, `<h1>Hello, World</h1>
-    <p>Go is running on Clever Cloud 💡☁️,</p>
-    <p>you should give it a try too!</p>`)
 
+	hostname, _ := os.Hostname()
+	port := os.Getenv("PORT")
+	instance := os.Getenv("INSTANCE_NUMBER")
+	now := time.Now().Format(time.RFC1123)
+
+	fmt.Fprintf(
+		w, `<h1>Hello, Youtube</h1>
+<p>Go is running on Clever Cloud 💡☁️</p>
+
+<p><b>Hostname:</b> %s</p>
+<p><b>Instance:</b> %s</p>
+<p><b>Port:</b> %s</p>
+<p><b>Server time:</b> %s</p>
+
+<p>Try it yourself on <a href="https://www.clever.cloud">Clever Cloud</a> 🚀</p>`,
+		hostname, instance, port, now,
+	)
 }
 
-//do anything
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
